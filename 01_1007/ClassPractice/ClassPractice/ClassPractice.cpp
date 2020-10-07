fe3377cb21203d0146aa9e9cf747237c3a184b97
Takuma-Shousuke <shousukesketty@gmail.com>
2020-10-07 13:37:28 +0900

    クラスの作り方の復習用プロジェクトを作成(バグあり)

---
+++ 01_1007/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,50 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+
+class Fighter
+{
+public:
+	Fighter();
+	~Fighter();
+
+	int m_hp;
+	int m_tec;
+	int m_luc;
+	int m_str;
+	int m_spd;
+	int m_mde;
+	int m_mag;
+	int m_def;
+};
+
+Fighter::Fighter()
+{
+	// 各変数を0初期化
+	m_hp  = 0;
+	m_tec = 0;
+	m_luc = 0;
+	m_str = 0;
+	m_spd = 0;
+	m_mde = 0;
+	m_mag = 0;
+	m_def = 0;
+}
+
+int main()
+{
+	Fighter fighter;
+	
+	// fighter内の各変数をprintfで出力する
+
+		printf("ファイターのHPは%dです\n" ,fighter.m_hp );
+		printf("ファイターのHPは%dです\n", fighter.m_tec);
+		printf("ファイターのHPは%dです\n", fighter.m_luc);
+		printf("ファイターのHPは%dです\n", fighter.m_str);
+		printf("ファイターのHPは%dです\n", fighter.m_spd);
+		printf("ファイターのHPは%dです\n", fighter.m_mde);
+		printf("ファイターのHPは%dです\n", fighter.m_mag);
+		printf("ファイターのHPは%dです\n", fighter.m_def);
+
+	system("pause");
+	return 0;
+}
